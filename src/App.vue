e1ea063d9d22480d7fcfdbfcf61d38720b5d305b
rdv-omsk <rdv-omsk@mail.ru>
2022-05-19 16:12:59 +0300

    new 8-2

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <form class="pt-3">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          class="form-control"
+          @blur="$v.email.$touch()"
+          v-model="email"
+        >
+      </div>
+
+      <pre>
+        {{ $v.email }}
+      </pre>
+
+    </form>
+  </div>
+</template>
+
+<script>
+import { required, email } from 'vuelidate/lib/validators'
+
+export default {
+  data () {
+    return {
+      email: ''
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
+    }
+  }
+}
+</script>
